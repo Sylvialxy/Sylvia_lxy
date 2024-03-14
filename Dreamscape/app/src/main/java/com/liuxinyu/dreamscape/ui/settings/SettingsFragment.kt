dc62b23c27ba3300cda50f1cb183b82065eb52cb
Sylvialxy <3379376146@qq.com>
2024-03-14 15:12:36 +0800

    4_2
    设置完善，但转向新的fragment还有点问题

--- Dreamscape/app/src/main/java/com/liuxinyu/dreamscape/ui/settings/SettingsFragment.kt
+++ Dreamscape/app/src/main/java/com/liuxinyu/dreamscape/ui/settings/SettingsFragment.kt
@@ -5,56 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import com.liuxinyu.dreamscape.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 设置菜单项点击监听器
+        val menuItem1: TextView = view.findViewById(R.id.menuItem1Text)
+        /*val menuItem2: TextView = view.findViewById(R.id.menuItem2)
+        val menuItem3: TextView = view.findViewById(R.id.menuItem3)
+        val menuItem4: TextView = view.findViewById(R.id.menuItem4)
+        val menuItem5: TextView = view.findViewById(R.id.menuItem5)
+        val menuItem6: TextView = view.findViewById(R.id.menuItem6)*/
+
+        menuItem1.setOnClickListener {
+            findNavController().navigate(R.id.action_settingsFragment_to_myFragment1)
+        }
+
+        /* menuItem2.setOnClickListener {
+             findNavController().navigate(R.id.action_settingsFragment_to_yourFragment2)
+         }
+
+         menuItem3.setOnClickListener {
+             findNavController().navigate(R.id.action_settingsFragment_to_yourFragment3)
+         }
+
+         menuItem4.setOnClickListener {
+             findNavController().navigate(R.id.action_settingsFragment_to_yourFragment4)
+         }
+
+         menuItem5.setOnClickListener {
+             findNavController().navigate(R.id.action_settingsFragment_to_yourFragment5)
+         }
+
+         menuItem6.setOnClickListener {
+             findNavController().navigate(R.id.action_settingsFragment_to_yourFragment6)
+         }*/
+
     }
+
+
 }
